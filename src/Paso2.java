49d642e320127ed0ee9b7ecff79dc8dacce46501
Guerola <antonioguerolacrespo@gmail.com>
2024-05-14 21:44:01 +0200

    Paso 4 de la actividad

--- src/Paso2.java
+++ src/Paso2.java
@@ -1,5 +1,7 @@
 public class Paso2 {
     public void printString(){
-        System.out.println("El paso de la actividad");
+        System.out.println("El paso 2 de la actividad");
+
+        System.out.println("Ahora vamos por el cuarto paso de la actividad");
     }
 }

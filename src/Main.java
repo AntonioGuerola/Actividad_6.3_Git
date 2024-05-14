095f6012d40f656f55a0e023af00a4a555798412
Guerola <antonioguerolacrespo@gmail.com>
2024-05-14 21:25:46 +0200

    Segundo paso de la actividad

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Paso 2 de la actividad 6.3 de entornos");
+
+        Paso2 paso2 = new Paso2();
+        paso2.printString();
+    }
+}
---
+++ src/Paso2.java
@@ -0,0 +1,5 @@
+public class Paso2 {
+    public void printString(){
+        System.out.println("El paso de la actividad");
+    }
+}
